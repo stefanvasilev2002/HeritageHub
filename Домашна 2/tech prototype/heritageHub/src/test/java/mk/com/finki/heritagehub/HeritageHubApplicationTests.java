db082351f664fe015d929031ad07959bc70390d7
stefance2002 <vasilevstefan2002@gmail.com>
2023-11-28 20:39:06 +0100

    Created folders and blank spring project

---
+++ Домашна 2/tech prototype/heritageHub/src/test/java/mk/com/finki/heritagehub/HeritageHubApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.com.finki.heritagehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeritageHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
