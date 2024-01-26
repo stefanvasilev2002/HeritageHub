160d56f59d55df0786c4e9a57a83ac8bab78d3fa
stefance2002 <vasilevstefan2002@gmail.com>
2024-01-26 01:46:42 +0100

    Postgre credentials update

--- heritageHub/src/main/java/com/finki/heritagehub/service/impl/MonumentServiceImpl.java
+++ heritageHub/src/main/java/com/finki/heritagehub/service/impl/MonumentServiceImpl.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
