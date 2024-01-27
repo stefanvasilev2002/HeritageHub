9d5cb3753be7172b512991b598dee8b649e62eaa
stefance2002 <vasilevstefan2002@gmail.com>
2024-01-27 13:28:20 +0100

    Transition to outlook smtp server

--- EmailMicroservice/src/main/java/com/finki/emailmicroservice/EmailMicroserviceApplication.java
+++ EmailMicroservice/src/main/java/com/finki/emailmicroservice/EmailMicroserviceApplication.java
@@ -2,6 +2,9 @@ package com.finki.emailmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootApplication
 public class EmailMicroserviceApplication {
@@ -9,5 +12,4 @@ public class EmailMicroserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmailMicroserviceApplication.class, args);
     }
-
 }
--- EmailMicroservice/src/main/java/com/finki/emailmicroservice/service/impl/EmailConfirmationServiceImpl.java
+++ EmailMicroservice/src/main/java/com/finki/emailmicroservice/service/impl/EmailConfirmationServiceImpl.java
@@ -1,46 +1,30 @@
 package com.finki.emailmicroservice.service.impl;
 
 import com.finki.emailmicroservice.service.EmailConfirmationService;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
 
-import java.io.IOException;
 
 @Service
 public class EmailConfirmationServiceImpl implements EmailConfirmationService {
-    @Value("${sendgrid.api-key}")
-    private String sendGridApiKey;
+    private final JavaMailSender javaMailSender;
+
+    public EmailConfirmationServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
     @Override
     public void sendConfirmationEmail(String toEmail, String confirmationLink) {
-        Email from = new Email("heritagehubmk@gmail.com");
-        Email to = new Email(toEmail);
-        String subject = "Confirm Your Registration";
-        String confirm = "<a href=" + confirmationLink + ">Confirm Your Account</a>";
-        Content content = new Content("text/html", "<p>Hello,</p>" +
-                "<p>Thank you for registering on our site, HeritageHubMK.</p>" +
-                "<p>Please click the following link to confirm your registration: </p>" + confirm
-                + "<p>If you didn't request this, please ignore this email. Your account will not be confirmed.</p>");
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(toEmail);
+        mailMessage.setSubject("Complete Registration!");
+        mailMessage.setFrom("heritagehubmk@outlook.com");
 
-        Mail mail = new Mail(from, subject, to, content);
-
-        SendGrid sg = new SendGrid(sendGridApiKey);
-        Request request = new Request();
-
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-
-            Response response = sg.api(request);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        String content = "Hello," +
+                "Thank you for registering on our site, HeritageHubMK." +
+                "Please click the following link to confirm your registration: " + confirmationLink +
+                "If you didn't request this, please ignore this email. Your account will not be confirmed.";
+        mailMessage.setText(content);
+        javaMailSender.send(mailMessage);
     }
 }
