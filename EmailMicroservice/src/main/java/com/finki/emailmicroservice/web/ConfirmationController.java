883126c4baa1fef270077cde22cee55cec0a7a2a
stefance2002 <vasilevstefan2002@gmail.com>
2024-01-28 21:00:31 +0100

    Home page

--- EmailMicroservice/src/main/java/com/finki/emailmicroservice/web/ConfirmationController.java
+++ EmailMicroservice/src/main/java/com/finki/emailmicroservice/web/ConfirmationController.java
@@ -3,10 +3,7 @@ package com.finki.emailmicroservice.web;
 import com.finki.emailmicroservice.model.ConfirmationRequest;
 import com.finki.emailmicroservice.service.EmailConfirmationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/confirmation")
@@ -23,5 +20,9 @@ public class ConfirmationController {
         emailConfirmationService.sendConfirmationEmail(confirmationRequest.getEmail(), confirmationRequest.getConfirmationLink());
 
         return ResponseEntity.ok("Confirmation email sent successfully.");
+    }
+    @GetMapping("/")
+    public ResponseEntity<String> home(){
+        return ResponseEntity.ok("Email confirmation microservice");
     }
 }
